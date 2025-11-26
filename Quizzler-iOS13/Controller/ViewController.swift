1b5a79d3b37c6621cec3e485ec85790f5786ff11
PSushant09 <sushantpatilios11@gmail.com>
2025-11-26 16:55:29 +0530

    Refactor for multiple-choice quiz support
    Updated the quiz app to support multiple-choice questions instead of true/false. Modified the Question model to hold an array of answers and the correct answer, updated QuizBrain logic, refactored ViewController to handle three answer buttons, and adjusted the storyboard layout accordingly.

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var scoresLbl: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
 
     var quizBrain = QuizBrain()
@@ -21,6 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
         progressBar.progress = 0
         questionLabel.text = quizBrain.getQuestionText()
     }
@@ -44,15 +46,22 @@ class ViewController: UIViewController {
             userInfo: nil,
             repeats: false
         )
-
     }
 
     @objc
     func updateUI() {
         questionLabel.text = quizBrain.getQuestionText()
         scoresLbl.text = "Score : \(quizBrain.getScore())"
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
+        
+        let answerChoice = quizBrain.getAnswers()
+        answerButton1.setTitle(answerChoice[0], for: .normal)
+        answerButton2.setTitle(answerChoice[1], for: .normal)
+        answerButton3.setTitle(answerChoice[2], for: .normal)
+        
+        answerButton1.backgroundColor = UIColor.clear
+        answerButton2.backgroundColor = UIColor.clear
+        answerButton3.backgroundColor = UIColor.clear
+        
         progressBar.progress = quizBrain.getProgress()
     }
 }
--- Quizzler-iOS13/Model/Question.swift
+++ Quizzler-iOS13/Model/Question.swift
@@ -10,10 +10,12 @@ import Foundation
 
 struct Question {
     var text: String
-    var answer: String
+    var answer: [String]
+    var rightAnswer: String
     
-    init(q: String, a: String) {
+    init(q: String, a: [String], correctAnswer: String) {
         self.text = q
         self.answer = a
+        self.rightAnswer = correctAnswer
     }
 }
